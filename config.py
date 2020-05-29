ca0696a90b0f31a1fa21b9c62334415add93ea93
Codefred <alfredemmanuelinyang@gmail.com>
2020-05-29 01:46:24 +0100

    update

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 class Config(object):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    MONGO_URI = '{}/{}'.format(os.getenv('DB_CONNECTION_URL'),os.getenv("DB_NAME"))
+    MONGO_URI = '{}/{}?retryWrites=true&w=majority'.format(os.getenv('DB_CONNECTION_URL'),os.getenv("DB_NAME"))
