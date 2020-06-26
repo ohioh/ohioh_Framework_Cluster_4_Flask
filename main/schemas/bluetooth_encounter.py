a1dcec0da2d5d5bf269f1d8bdb6154e13a98b390
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-26 22:24:24 +0100

    update defaults

--- main/schemas/bluetooth_encounter.py
+++ main/schemas/bluetooth_encounter.py
@@ -7,7 +7,7 @@ import marshmallow.fields as ms_fields
 class BluetoothEncounterSchema(Schema):
     user_id = ms_fields.Str(required=True)
     user_timestamp = ms_fields.DateTime(default=datetime.now())
-    encounter_user_id = ms_fields.Str()
+    encounter_user_id = ms_fields.Str(default="")
 
     class Meta:
         unknown = EXCLUDE
